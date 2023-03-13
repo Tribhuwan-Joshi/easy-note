16bf093bd038e941cb830d02eea00bc6f5943a98
Tribhuwan <mathincosmos@gmail.com>
2023-03-13 16:54:53 +0530

    style note component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,15 +1,29 @@
-import {Card,CardContent,Typography,Box,Button} from "@mui/material"
+import {Card,CardContent,Typography,Box,Button,styled} from "@mui/material"
 import { NoteObject } from '../models/note'
+const styledCard = styled(Card)`
+width:400px;
+margin:20px;
+`
 
+const Wrapper = styled(Box)`
+& > button {
+    border: 1px solid black;
+    background:white;
+    margin-top:5px;
+
+}
+`
 function Note({note}:{note:NoteObject}) {
   return (
-    <Card style={{backgroundColor:note.color}}>
+    <Card style={{ backgroundColor: note.color }}>
       <CardContent>
         <Box>
           <Typography>{note.title}</Typography>
           <Typography>{note.details}</Typography>
           <Typography>{note.date}</Typography>
-          <Button variant="outlined">Delete</Button>
+          <Wrapper>
+            <Button variant="outlined">Delete</Button>
+          </Wrapper>
         </Box>
       </CardContent>
     </Card>
