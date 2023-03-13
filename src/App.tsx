e3ea41d8118cd8d9aff50f6a91d248e6e2730ca0
Tribhuwan <mathincosmos@gmail.com>
2023-03-13 17:16:11 +0530

    use of localStorage api to save notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import CreateNote from "./components/CreateNote";
 import Header from "./components/Header";
-import {Box} from "@mui/material"
+import { Box } from "@mui/material";
 import Notes from "./components/Notes";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NoteObject } from "./models/note";
 function App() {
-  const [notes,setNotes] = useState<NoteObject[]>([]);
-  const addNotes = (note : NoteObject) => {
-    setNotes([note,...notes])
-  }
-      function deleteNote(id:number){
-setNotes(notes => notes.filter(note => note.id!==id));
+  const [notes, setNotes] = useState<NoteObject[]>([]);
+  useEffect(()=>{
+    if(localStorage.getItem("notes")){
+      setNotes(JSON.parse(localStorage.getItem("notes") as string));
+
     }
+  },[])
+  const addNotes = (note: NoteObject) => {
+    setNotes([note, ...notes]);
+    localStorage.setItem("notes", JSON.stringify([note, ...notes]));
+  };
+  function deleteNote(id: number) {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+  }
   return (
     <>
       <Header />
       <Box style={{ padding: 20 }}>
         <CreateNote addNotes={addNotes} />
-        <Notes notes={notes} deleteNote={deleteNote}/>
+        <Notes notes={notes} deleteNote={deleteNote} />
       </Box>
     </>
   );
